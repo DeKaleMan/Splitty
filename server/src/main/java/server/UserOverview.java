d823b2f145113e4f72b83391e8a989e1632b01cb
Joris Timmermans <j.h.m.timmermans@student.tudelft.nl>
2024-02-24 13:49:30 +0100

    UserOverview class

---
+++ server/src/main/java/server/UserOverview.java
@@ -0,0 +1,38 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserOverview {
+
+    //private final EventRepository repo;
+    private String name;
+    private String email;
+    private List<String> events;
+    private UserOverview(String name, String email){
+        this.name = name;
+        this.email = email;
+        this.events = new ArrayList<>(); //add all events here
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<String> getEvents() {
+        return events;
+    }
+
+    public boolean changeTitleEvent(String event, String title){
+        // return event.setTitle(title);
+        return true;
+    }
+    public List<String> getExpenses(String event){
+        List<String> res = new ArrayList<>(); //event.getExpenses();
+        return res;
+    }
+}
