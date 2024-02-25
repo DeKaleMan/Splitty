46f1828aa489df0fd1c32ea9cf9f8c6784c8ba5b
Jesse Daniël Prins <j.d.prins@student.tudelft.nl>
2024-02-25 14:03:17 +0100

    Created an equals and hashcode for ConversionResponse (last commmit I hope)

--- server/src/main/java/server/util/ConversionResponse.java
+++ server/src/main/java/server/util/ConversionResponse.java
@@ -1,5 +1,7 @@
 package server.util;
 
+import java.util.Objects;
+
 public class ConversionResponse {
 
     private double amount;
@@ -24,5 +26,18 @@ public class ConversionResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConversionResponse that = (ConversionResponse) o;
+        return Double.compare(that.amount, amount) == 0 && Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, message);
     }
 }
