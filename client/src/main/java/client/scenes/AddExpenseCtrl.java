d9300632d738aafb4d1130835737958232648825
JulBeast02 <overmars.julian@gmail.com>
2024-02-27 15:35:57 +0100

    added 3 back buttons and set titles when switching between pages

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -1,6 +1,8 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import javax.inject.Inject;
 
@@ -8,9 +10,25 @@ public class AddExpenseCtrl {
     private final ServerUtils serverUtils;
     private final MainCtrl mainCtrl;
 
+    @FXML
+    public Label titleLabel;
+
     @Inject
     public AddExpenseCtrl(ServerUtils serverUtils, MainCtrl mainCtrl) {
         this.serverUtils = serverUtils;
         this.mainCtrl = mainCtrl;
+    }
+
+    @FXML
+    public void back() {
+        mainCtrl.showSplittyOverview(titleLabel.getText());
+    }
+
+    /**
+     * Sets the title of the event
+     * @param title event's title
+     */
+    public void setTitle(String title){
+        titleLabel.setText(title);
     }
 }
