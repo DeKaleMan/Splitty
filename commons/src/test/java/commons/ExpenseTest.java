10b1151fd799102c076dc482dbc5387b8cf26328
Maciej <M.Kozik@student.tudelft.nl>
2024-02-29 19:34:48 +0100

    Modified Expense class to JPA standards

--- commons/src/test/java/commons/ExpenseTest.java
+++ commons/src/test/java/commons/ExpenseTest.java
@@ -1,10 +1,7 @@
 package commons;
 
 import org.junit.jupiter.api.Test;
-import commons.Currency;
-import commons.Expense;
-import commons.Transaction;
-import commons.Type;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,28 +17,28 @@ public class ExpenseTest {
     Transaction t6 = new Transaction("Jakub", -25.0);
     List<Transaction> transactions = List.of(t1, t2, t3, t4, t5, t6);
     List<Transaction> transactionsMinusOne = List.of(t1, t2, t3, t4, t5);
-    Expense expense = new Expense("This is a test expense", transactions, Type.Food,
-            Currency.EUR, "24-02-2024", 100.50, "John");
+    Expense expense = new Expense("This is a test expense", Type.Food,
+        Currency.EUR, "24-02-2024", 100.50, "John");
 
-    Expense expenseCopy = new Expense("This is a test expense", transactions, Type.Food,
-            Currency.EUR, "24-02-2024", 100.50, "John");
-    Expense expenseFalseCopy = new Expense("This is a test expense", transactionsMinusOne, Type.Food,
-            Currency.EUR, "24-02-2024", 100.50, "John");
+    Expense expenseCopy = new Expense("This is a test expense", Type.Food,
+        Currency.EUR, "24-02-2024", 100.50, "John");
+    Expense expense2 = new Expense("This is a test expense", Type.Drinks,
+        Currency.EUR, "24-02-2024", 120.50, "Edgar");
 
     @Test
     public void stringTest() {
         String s = "This is an expense:\n" +
-                "This is a test expense\n" +
-                "The expense type is: Food.\n" +
-                "The total amount spent is: 100.5.\n" +
-                "This is how much everyone owes:\n" +
-                "\tJohn: 100.5.\n" +
-                "\tCyndia: -20.5.\n" +
-                "\tNikolay: -10.0.\n" +
-                "\tRobin: -40.0.\n" +
-                "\tMarshall: -5.0.\n" +
-                "\tJakub: -25.0.\n" +
-                "The person who paid was: John, on 24-02-2024 and paid in EUR.";
+            "This is a test expense\n" +
+            "The expense type is: Food.\n" +
+            "The total amount spent is: 100.5.\n" +
+//                "This is how much everyone owes:\n" +
+//                "\tJohn: 100.5.\n" +
+//                "\tCyndia: -20.5.\n" +
+//                "\tNikolay: -10.0.\n" +
+//                "\tRobin: -40.0.\n" +
+//                "\tMarshall: -5.0.\n" +
+//                "\tJakub: -25.0.\n" +
+            "The person who paid was: John, on 24-02-2024 and paid in EUR.";
         assertEquals(s, expense.toString());
     }
 
@@ -63,7 +60,7 @@ public class ExpenseTest {
 
     @Test
     public void notEquals2() {
-        assertNotEquals(expense, expenseFalseCopy);
+        assertNotEquals(expense, expense2);
     }
 
     @Test
