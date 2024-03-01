c702be9bb63ec70502c1e5e43647110af9ef1169
Jesse Daniël Prins <j.d.prins@student.tudelft.nl>
2024-03-01 19:14:24 +0100

    Test mock classes and first tests

--- server/src/test/java/server/api/CurrencyExchangeTest.java
+++ server/src/test/java/server/api/CurrencyExchangeTest.java
@@ -1,5 +1,6 @@
 package server.api;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,16 +12,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CurrencyExchangeTest {
 
+    private CurrencyExchange currencyExchange;
+
+    @BeforeEach
+    void initialize() {
+        currencyExchange = new CurrencyExchange(new IOUtilsTest(), new HttpResponseTest());
+    }
+
+
     @Test
     void invalidDoubleTest() {
-        CurrencyExchange currencyExchange = new CurrencyExchange(new IOUtilsTest(), new HttpResponseTest());
         ResponseEntity<ConversionResponse> response = currencyExchange.getConvertedAmount("eurusd", "50klj");
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 
     @Test
     void invalidConversionTest() {
-        CurrencyExchange currencyExchange = new CurrencyExchange(new IOUtilsTest(), new HttpResponseTest());
         ResponseEntity<ConversionResponse> response = currencyExchange.getConvertedAmount("eurucd", "50");
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
