58523a7e645be71297ff54bcde8ec886b5b91c44
Yavor Pachedzhiev <Y.S.Pachedzhiev-1@student.tudelft.nl>
2024-03-03 15:57:59 +0100

    checkstyle

--- commons/src/test/java/commons/ParticipantTest.java
+++ commons/src/test/java/commons/ParticipantTest.java
@@ -33,7 +33,9 @@ class ParticipantTest {
                 "Yavor", "yavor@tudelft.nl");
         Participant participant2 = new Participant("Yavor", 9876.54,
                 "GB29NWBK60161331926819", "NWBKGB22XXX", "Yavor", "yavor@tudelft.nl");
-        Participant participant3 = new Participant("Jesse", 8765.43, "FR1420041010050500013M02606", "PSSTFRPPXXX", "Jesse", "jesse@tudelft.nl");
+        Participant participant3 = new Participant("Jesse", 8765.43,
+                "FR1420041010050500013M02606", "PSSTFRPPXXX",
+                "Jesse", "jesse@tudelft.nl");
 
         assertAll("Testing equals",
                 () -> assertEquals(participant1, participant2,
