3dc29110dfd09fa31a209875c6d82774de92f1c8
Jesse Daniël Prins <j.d.prins@student.tudelft.nl>
2024-03-04 23:14:59 +0100

    admin management overview preparation to actually make it functional

--- client/src/main/java/client/scenes/AdminOverviewCtrl.java
+++ client/src/main/java/client/scenes/AdminOverviewCtrl.java
@@ -7,12 +7,21 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 public class AdminOverviewCtrl {
 
     private MainCtrl mainCtrl;
 
     ObservableList<String> sortList = FXCollections.observableArrayList("Title", "Creation date", "Last activity");
+
+    // This is test code and should be replaced using api requests
+    ObservableList<Event> testEventList = FXCollections.observableArrayList(
+            new Event("Test", "10-11-2005", "Jesse", "testdesc"),
+            new Event("Test1", "10-11-2005", "Jesse", "testdesc"),
+            new Event("Test3", "10-11-2005", "Jesse", "testdesc"),
+            new Event("Test4", "10-11-2005", "Jesse", "testdesc"),
+            new Event("Test5", "10-11-2005", "Jesse", "testdesc"));
 
     @FXML
     private Button importEventButton;
@@ -49,6 +58,27 @@ public class AdminOverviewCtrl {
     @FXML
     public void initialize() {
         sortComboBox.setItems(sortList);
+
+        // This code (It took me some googling to find this out) makes sure that the items
+        // that are shown in the listview only show the event name, the default is the toString method
+        eventList.setCellFactory(new Callback<>() {
+            @Override
+            public ListCell<Event> call(ListView<Event> listView) {
+                return new ListCell<>() {
+                    @Override
+                    protected void updateItem(Event event, boolean empty) {
+                        super.updateItem(event, empty);
+                        if (empty || event == null) {
+                            setText(null);
+                        } else {
+                            setText(event.getName());
+                        }
+                    }
+                };
+            }
+        });
+
+        eventList.setItems(testEventList);
     }
 
     @FXML
@@ -73,16 +103,19 @@ public class AdminOverviewCtrl {
     @FXML
     public void exportEvent() {
         Event event = eventList.getSelectionModel().getSelectedItem();
+        System.out.println("Export: " + event);
     }
 
     @FXML
     public void deleteEvent() {
         Event event = eventList.getSelectionModel().getSelectedItem();
+        System.out.println("Delete: " + event);
     }
 
     @FXML
     public void viewEvent() {
         Event event = eventList.getSelectionModel().getSelectedItem();
+        System.out.println("View: " + event);
     }
 
     @FXML
@@ -100,5 +133,10 @@ public class AdminOverviewCtrl {
                 System.out.println("Last activity sorting selected");
                 break;
         }
+    }
+
+    @FXML
+    public void logOut() {
+        mainCtrl.showStartScreen();
     }
 }
