d93d3e6394a248b3bd481c2f798de9f7a1140958
Yavor Pachedzhiev <Y.S.Pachedzhiev-1@student.tudelft.nl>
2024-03-07 03:47:58 +0100

    minor fix

--- commons/src/test/java/commons/DebtTest.java
+++ commons/src/test/java/commons/DebtTest.java
@@ -9,7 +9,7 @@ public class DebtTest {
     double b1 = 100.50;
     double b2 = -20.00;
     Expense e1 = new Expense(100, "food1", Type.Food, Currency.EUR, "16 March", 150.00, "test@student.lentiz.nl");
-    Participant p1 = new Participant("stijn", -500.00, "NL83 INGB 0743 7843", "9843", "R.B. de Jong", "test@gmail.com");
+    Participant p1 = new Participant("stijn", -500.00, "NL83 INGB 0743 7843", "9843", "R.B. de Jong", "test@gmail.com", new Event());
     Debt t1 = new Debt(e1, b1, p1);
     Debt t2 = new Debt(e1, b1, p1);
 
