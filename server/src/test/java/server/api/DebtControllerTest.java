911b2373491011827eede5f2e24a53dbca5ccf6e
Maciej <M.Kozik@student.tudelft.nl>
2024-03-13 22:20:22 +0100

    Added empty testing

---
+++ server/src/test/java/server/api/DebtControllerTest.java
@@ -0,0 +1,14 @@
+package server.api;
+
+import org.junit.jupiter.api.Test;
+import server.database.DebtRepository;
+import server.database.ExpenseRepository;
+import server.database.ParticipantRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DebtControllerTest {
+    DebtRepository debtRepository = new TestDebtRepository();
+
+
+}
