de922427e403620615a317cd13d8f8a183c42ae0
Yavor Pachedzhiev <Y.S.Pachedzhiev-1@student.tudelft.nl>
2024-03-17 21:24:51 +0100

    add participant tests

--- commons/src/main/java/commons/Participant.java
+++ commons/src/main/java/commons/Participant.java
@@ -97,6 +97,10 @@ public class Participant implements Serializable {
         return id.getEvent();
     }
 
+    public ParticipantId getId() {
+        return id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
