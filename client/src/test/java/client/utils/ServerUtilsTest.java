d380efefcc69696f746de8ee19238f1de98507a7
stijn de Lange <c.s.delange@student.tudelft.nl>
2024-03-23 15:26:00 +0100

    quick commit to change to other branch

--- client/src/test/java/client/utils/ServerUtilsTest.java
+++ client/src/test/java/client/utils/ServerUtilsTest.java
@@ -4,6 +4,7 @@ package client.utils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
