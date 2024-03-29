6b071ad6bd0c3ccb63a1be6ef662fd651688b0d6
Julian Overmars <j.l.overmars@student.tudelft.nl>
2024-03-29 16:28:34 +0100

    added error for name

--- client/src/main/java/client/scenes/CreateEventCtrl.java
+++ client/src/main/java/client/scenes/CreateEventCtrl.java
@@ -17,7 +17,6 @@ import javafx.scene.input.KeyEvent;
 import javax.inject.Inject;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -26,6 +25,7 @@ public class CreateEventCtrl {
     private final ServerUtils serverUtils;
     private final MainCtrl mainCtrl;
     private final Config config;
+
 
 
     // event text fields
@@ -48,6 +48,8 @@ public class CreateEventCtrl {
 
     @FXML
     private TextField nameField;
+    @FXML
+    public Label hostNameError;
     @FXML
     private TextField emailField;
     @FXML
@@ -82,45 +84,15 @@ public class CreateEventCtrl {
     }
 
     @FXML
-    public void addParticipant() {
-        String name = nameField.getText();
-        String email = emailField.getText();
-        String iBan = iBanField.getText();
-        String bIC = bICField.getText();
-        String accountHolder = accountHolderField.getText();
-
-        if     (name == null || name.isEmpty() ||
-                email == null || email.isEmpty() ||
-                iBan == null || iBan.isEmpty() ||
-                bIC == null || bIC.isEmpty() ||
-                accountHolder == null || accountHolder.isEmpty()) {
-            // give warning *every field must be filled in*
-            return;
-        }
-//        Participant participant = new Participant(name, 0.0, iBan, bIC, accountHolder, email);
-//        participants.add(participant);
-
-        nameField.setText("");
-        emailField.setText("");
-        iBanField.setText("");
-        bICField.setText("");
-        accountHolderField.setText("");
+    public ParticipantDTO addHost(int id) {
+        return new ParticipantDTO(nameField.getText(), 0.0, config.getIban(), config.getBic(),
+                config.getEmail(), config.getName(), id, config.getId());
     }
 
     @FXML
     public void createEvent() {
         String name = titleField.getText();
         String dateString = datePicker.getEditor().getText();
-        if(dateString.isBlank()||dateString.isEmpty()) {
-            datePicker.setPromptText("Invalid date");
-            datePicker.setValue(null);
-            return;
-        }
-        if(name.isEmpty()||name.isBlank()){
-            titleField.setPromptText("invalid name");
-            titleField.setText("");
-            return;
-        }
         LocalDate localDate = datePicker.getValue();
         String description = eventDescriptionArea.getText();
         Date date = null;
@@ -130,30 +102,31 @@ public class CreateEventCtrl {
                 throw new IllegalArgumentException();
             }
             date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        } catch (NumberFormatException | IndexOutOfBoundsException | DateTimeParseException e) {
-            dateEmptyError.setVisible(false);
-            dateIncorrectError.setVisible(true);
-            error = true;
         } catch (IllegalArgumentException e) {
             dateIncorrectError.setVisible(false);
             dateEmptyError.setVisible(true);
+            error = true;
+        } catch (Exception e) {
+            dateEmptyError.setVisible(false);
+            dateIncorrectError.setVisible(true);
             error = true;
         }
         if (name == null || name.isEmpty() || error){
             if (name == null || name.isEmpty()) {
                 titleError.setVisible(true);
             }
+            if (nameField.getText() == null || nameField.getText().isEmpty()) {
+                hostNameError.setVisible(true);
+            }
             return;
         }
+
         //fetch owner
         String owner = config.getId();
-
-        //System.out.println("Created new event: " + name);
         // create new event and add to database, go to that event overview and add participants via database.
         EventDTO event = new EventDTO(name, date, owner, description);
         Event eventCreated = serverUtils.addEvent(event);
-        ParticipantDTO participantDTO = new ParticipantDTO(nameField.getText(), 0.0, config.getIban(), config.getBic(),
-                config.getEmail(), config.getName(), eventCreated.getId(), config.getId());
+        ParticipantDTO participantDTO = addHost(eventCreated.getId());
         serverUtils.createParticipant(participantDTO);
         mainCtrl.showSplittyOverview(eventCreated.getId());
     }
